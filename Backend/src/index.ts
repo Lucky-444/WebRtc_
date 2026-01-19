9dd251122e48ed4805f753911c6d22e8988b9651
Lucky <sashankswain6@gmail.com>
2026-01-20 03:30:01 +0530

    Added Room Logic and join Logic

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import ServerConfig from "../config/ServerConfig";
+import ServerConfig from "./config/ServerConfig";
 import { Server } from "socket.io";
 import http from "http";
 import cors from "cors";
+import roomHandler from "./handlers/roomHandler";
 
 const app = express();
 
@@ -19,7 +20,7 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log("New user connected");
-
+  roomHandler(socket); //pass the socket connection to the room handler for room creation and joining
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
