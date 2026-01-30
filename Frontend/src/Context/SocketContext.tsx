b2b38be770865d07092dcf92d41789ed70f1dc2b
Lucky <sashankswain6@gmail.com>
2026-01-31 03:07:03 +0530

    adding Stream using Media stream

--- Frontend/src/Context/SocketContext.tsx
+++ Frontend/src/Context/SocketContext.tsx
@@ -1,11 +1,8 @@
 import SocketIoClient from "socket.io-client";
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Peer from "peerjs"
-import {v4 as UUIDv4} from "uuid";
-
-
-
+import Peer from "peerjs";
+import { v4 as UUIDv4 } from "uuid";
 
 const wsServer = "http://localhost:8080";
 
@@ -14,18 +11,18 @@ export const socketContext = createContext<any | null>(null);
 const socket = SocketIoClient(wsServer, {
   withCredentials: false,
   transports: ["polling", "websocket"],
-});;
+});
 
 interface props {
   children: React.ReactNode;
-};
+}
 
 const SocketProvider: React.FC<props> = ({ children }) => {
   const navigate = useNavigate();
 
-
   //State Variable to store the UserId
-  const [user , setUser] = useState<Peer>();
+  const [user, setUser] = useState<Peer>();
+  const [stream, setStream] = useState<MediaStream>();
 
   const fetchParticpantsList = ({
     roomId,
@@ -38,39 +35,35 @@ const SocketProvider: React.FC<props> = ({ children }) => {
     console.log(roomId, particpants);
   };
 
-
-
-
-
-
-
-
-
+  const fetchUserFeed = async () => {
+    const st = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    setStream(st);
+  };
 
   useEffect(() => {
-
     const userId = UUIDv4();
     const newPeer = new Peer(userId);
 
     setUser(newPeer);
 
+    fetchUserFeed();
 
+    const enterRoom = ({ roomId }: { roomId: string }) => {
+      navigate(`/room/${roomId}`);
+    };
 
-
-    const enterRoom = ({roomId} : {roomId : string}) => {
-      navigate(`/room/${roomId}`)
-    }
-    
     //We will transfer the user to the room Page when we Collect the an Event room created From Server
-    socket.on("room-created" , enterRoom);
+    socket.on("room-created", enterRoom);
 
-    socket.on("get-users" , fetchParticpantsList)
-  }, [])
-
+    socket.on("get-users", fetchParticpantsList);
+  }, []);
 
   return (
-    <socketContext.Provider value={{socket, user}} >
-      {children }
+    <socketContext.Provider value={{ socket, user }}>
+      {children}
     </socketContext.Provider>
   );
 };
